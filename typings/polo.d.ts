65772c4e54b52c489653c6a411466e9205f111f1
nspeedzy <ninjasurya71@gmail.com>
2020-08-19 22:31:00 +0530

    Second commit

--- typings/polo.d.ts
+++ typings/polo.d.ts
@@ -1,3 +1,10 @@
-export declare function add(a: number, b: number): number;
+export declare namespace logical {
+    function add(a: number, b: number): number;
+    function subtract(a: number, b: number): number;
+    function multiply(a: number, b: number): number;
+    function divide(a: number, b: number): number;
+}
+export declare class AssertiveError {
+}
 export declare const version: string;
 //# sourceMappingURL=polo.d.ts.map
