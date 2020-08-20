7b461fb0d5da1393dd3ca9c1804533748d1ec47f
nspeedzy <ninjasurya71@gmail.com>
2020-08-20 12:04:48 +0530

    Second commit

--- lib/polo.d.ts
+++ lib/polo.d.ts
@@ -1,2 +1,42 @@
-declare function add(...params: any[]): any[];
-export { add };
+/**
+ * Provides useful functions related to
+ * arithmetic maths
+ */
+export declare namespace arithmetic {
+    /**
+     * Adds two numbers together and returns the
+     * sum of the two numbers
+     * @param {number} a The first number
+     * @param {number} b The second number
+     * @returns {number} The sum of the two numbers specified
+     */
+    function add(a: number, b: number): number;
+    /**
+     * Subtracts two numbers together and returns
+     * the difference between them
+     * @param {number} a The first number
+     * @param {number} b The second number
+     * @returns {number} The sum of the two numbers specified
+     */
+    function subtract(a: number, b: number): number;
+    /**
+     * Multiplies two numbers together and returns
+     * the result
+     * @param {number} a The first number
+     * @param {number} b The second number
+     * @returns {number} The result from the two numbers specified
+     */
+    function multiply(a: number, b: number): number;
+    /**
+     * Divides two numbers together and returns
+     * the difference between them
+     * @param {number} a The first number
+     * @param {number} b The second number
+     * @returns {number} The result from the two numbers specified
+     */
+    function divide(a: number, b: number): number;
+}
+/**
+ * The version of the library
+ */
+export declare const version: string;
--- src/polo.ts
+++ src/polo.ts
@@ -1,6 +1,57 @@
-function add(...params: any[]) {
-    console.log(params);
-    return params;
+/**
+ * Provides useful functions related to
+ * arithmetic maths
+ */
+export namespace arithmetic {
+    /**
+     * Adds two numbers together and returns the 
+     * sum of the two numbers
+     * @param {number} a The first number 
+     * @param {number} b The second number
+     * @returns {number} The sum of the two numbers specified
+     */
+    export function add(a: number, b: number): number {
+        return a + b;
+    }
+    /**
+     * Subtracts two numbers together and returns 
+     * the difference between them
+     * @param {number} a The first number 
+     * @param {number} b The second number
+     * @returns {number} The sum of the two numbers specified
+     */
+    export function subtract(a: number, b: number): number {
+        return a - b;
+    }
+    /**
+     * Multiplies two numbers together and returns 
+     * the result
+     * @param {number} a The first number 
+     * @param {number} b The second number
+     * @returns {number} The result from the two numbers specified
+     */
+    export function multiply(a: number, b: number): number {
+        return a * b;
+    }
+    /**
+     * Divides two numbers together and returns 
+     * the difference between them
+     * @param {number} a The first number 
+     * @param {number} b The second number
+     * @returns {number} The result from the two numbers specified
+     */
+    export function divide(a: number, b: number): number {
+        return a / b;
+    }
 }
 //
-export { add };
+class Logical {
+    constructor() {
+        //
+    }
+}
+//
+/**
+ * The version of the library
+ */
+export const version: string = "1.0.0";
