1b142c00d2bfedcb0e506a6d38e8e5bbb4e4f8ba
nspeedzy <ninjasurya71@gmail.com>
2020-08-20 12:22:15 +0530

    Second commit

--- lib/polo.d.ts
+++ lib/polo.d.ts
@@ -1,3 +1,6 @@
 declare module "polo.js" {
     export const version: string;
+    export function add(a: number, b: number): number;
+    export namespace runtime {  }
+    export interface Options {  }
 }
--- src/polo.ts
+++ src/polo.ts
@@ -1,3 +1,6 @@
-module polo {
-    export const version: string = "Ambient";
+export const version: string = "1.0.0";
+export function add(a: number, b: number): number {
+    return a + b;
 }
+export namespace runtime {}
+export interface Options {}
