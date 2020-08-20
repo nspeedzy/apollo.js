57899b001a6ee88db66a3bef15912e88696dc07d
nspeedzy <ninjasurya71@gmail.com>
2020-08-20 14:29:01 +0530

    Second commit

--- lib/polo.d.ts
+++ lib/polo.d.ts
@@ -1,6 +1,99 @@
 declare module "polo.js" {
+
+    import {
+        Client,
+        ClientOptions,
+        Guild,
+        User
+    } from "discord.js";
+
     export const version: string;
-    export function add(a: number, b: number): number;
-    export namespace runtime {  }
-    export interface Options {  }
+
+//#region TypeDefs
+
+    export type ReadyEvent = string | ((client: PoloClient) => string);
+
+    /**
+     * PoloClient options
+     */
+    export interface PoloClientOptions extends ClientOptions {
+        /**
+         * The prefix(es) for the bot
+         */
+        prefix?: string | string[];
+        /**
+         * Specify whether to make the prefix case insensitive or not
+         * Default is false
+         */
+        prefixCaseInsensitive?: boolean;
+        /**
+         * Executed when Client is ready
+         */
+        onready?: ReadyEvent;
+        /**
+         * The bot owner(s). Please make sure you specify the user's tag
+         */
+        owners?: string | string[];
+        /**
+         * The general options for polo
+         */
+        polodefaults?: PoloOptions;
+        /**
+         * When set to `true`, The client will show that the bot is 
+         * typing. Default is `false`.
+         */
+        typing?: boolean;
+        /**
+         * The language of the bot
+         */
+        language?: string;
+        /**
+         * Specify whether to disable all Polo defaults. Default is
+         * `false`
+         */
+        disableAllDefaults: boolean;
+    }
+
+    /**
+     * General polo potions
+     */
+    export interface PoloOptions {  }
+
+//#region Classes
+
+    export class PoloClient extends Client {
+        /**
+         * The `PoloClient` constructor
+         * @param {PoloClientOptions} options The desired options you want for your bot
+         */
+        public constructor(options?: PoloClientOptions);
+        /**
+         * Logs the client in, establishing a websocket connection to Discord.
+         * @param {string} token Token of the account to log in with
+         * @see https://discord.js.org/#/docs/main/stable/class/Client?scrollTo=login
+         */
+        public login(token?: string): Promise<string>;
+    }
+
+    export { PoloClient as Client };
+
+    /**
+     * PoloError class for errors based on polo.js
+     */
+    export class PoloError extends Error {
+        /**
+         * The PoloError constructor
+         * @param {string} type The type of the error
+         * @param {string} msg The error message to be thrown
+         */
+        public constructor(type: string, msg: string);
+        /**
+         * Logs just the message into the console
+         */
+        public throw(): void;
+    }
+
+    export class PoloGuild extends Guild {  }
+
+    export class PoloUser extends User {  }
 }
--- src/polo.ts
+++ src/polo.ts
@@ -1,6 +1,38 @@
+import {
+    Client, ClientOptions,
+    Guild, User
+} from "discord.js";
 export const version: string = "1.0.0";
-export function add(a: number, b: number): number {
-    return a + b;
+//#region TypeDefs
+export type ReadyEvent = string | ((client: PoloClient) => string);
+export interface PoloClientOptions extends ClientOptions {
+    prefix?: string | string[];
+    prefixCaseInsensitive?: boolean;
+    onready?: ReadyEvent;
+    owners?: string | string[];
+    polodefaults?: PoloOptions;
+    typing?: boolean;
+    language?: string;
+    disableAllDefaults: boolean;
 }
-export namespace runtime {}
-export interface Options {}
+export interface PoloOptions {}
+//#region Classes
+export class PoloClient extends Client {
+    public constructor(options?: PoloClientOptions) {
+        super(options);
+    }
+    public async login(token: string) {
+        return token;
+    }
+}
+export class PoloError extends Error {
+    constructor(type: string, msg: string) {
+        super(msg);
+        this.name = type;
+    }
+    public throw(): void {
+        console.error(this.message);
+    }
+}
+export class PoloGuild extends Guild {}
+export class PoloUser extends User {}
